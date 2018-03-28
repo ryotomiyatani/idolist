f9d4a5402e4bf70f82eafa899c69b376c9776a4e
miyatani <ec2-user@ip-172-31-5-122.us-east-2.compute.internal>
2018-03-28 10:54:17 +0000

    order

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,30 +1,30 @@
 class ToppagesController < ApplicationController
   def index
-           @idols = Idol.all.order(:idol_namekana).page(params[:page]).per(100)
+           @idols = Idol.all.page(params[:page]).order(:idol_namekana).per(100)
         if params[:name].present? 
-          @idols = Idol.order(:idol_namekana).get_by_idol_name(params[:name]).page(params[:page]).per(100)
+          @idols = Idol.get_by_idol_name(params[:name]).order(:idol_namekana).page(params[:page]).per(100)
         end
 
         if params[:name_a] == "あ"
-          @idols = Idol.order(:idol_namekana).get_by_name_a("あ%").page(params[:page]).per(100)
+          @idols = Idol.get_by_name_a("あ%").order(:idol_namekana).page(params[:page]).per(100)
         elsif params[:name_ka] == "か"
-          @idols = Idol.order(:idol_namekana).get_by_name_ka("か%").page(params[:page]).per(100)
+          @idols = Idol.get_by_name_ka("か%").order(:idol_namekana).page(params[:page]).per(100)
         elsif params[:name_sa] == "さ"
-          @idols = Idol.order(:idol_namekana).get_by_name_sa("さ%").page(params[:page]).per(100)
+          @idols = Idol.get_by_name_sa("さ%").order(:idol_namekana).page(params[:page]).per(100)
         elsif params[:name_ta] == "た"
-          @idols = Idol.order(:idol_namekana).get_by_name_ta("た%").page(params[:page]).per(100)
+          @idols = Idol..get_by_name_ta("た%").order(:idol_namekana).page(params[:page]).per(100)
         elsif params[:name_na] == "な"
-          @idols = Idol.order(:idol_namekana).get_by_name_na("な%").page(params[:page]).per(100)
+          @idols = Idol.get_by_name_na("な%").order(:idol_namekana).page(params[:page]).per(100)
         elsif params[:name_ha] == "は"
-          @idols = Idol.order(:idol_namekana).get_by_name_ha("は%").page(params[:page]).per(100)
+          @idols = Idol.get_by_name_ha("は%").order(:idol_namekana).page(params[:page]).per(100)
         elsif params[:name_ma] == "ま"
-          @idols = Idol.order(:idol_namekana).get_by_name_ma("ま%").page(params[:page]).per(100)
+          @idols = Idol.get_by_name_ma("ま%").order(:idol_namekana).page(params[:page]).per(100)
         elsif params[:name_ya] == "や"
-          @idols = Idol.order(:idol_namekana).get_by_name_ya("や%").page(params[:page]).per(100)
+          @idols = Idol.get_by_name_ya("や%").order(:idol_namekana).page(params[:page]).per(100)
         elsif params[:name_ra] == "ら"
-          @idols = Idol.order(:idol_namekana).get_by_name_ra("ら%").page(params[:page]).per(100)
+          @idols = Idol.get_by_name_ra("ら%").order(:idol_namekana).page(params[:page]).per(100)
         elsif params[:name_wa] == "わ"
-          @idols = Idol.order(:idol_namekana).get_by_name_wa("わ%").page(params[:page]).per(100)
+          @idols = Idol.get_by_name_wa("わ%").order(:idol_namekana).page(params[:page]).per(100)
       end
   end
 end
